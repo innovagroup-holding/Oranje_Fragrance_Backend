aac437c1d2e3ba1843db6665736361ffd8216052
Rama Abbas <r.abbas@sahmtech.com>
2024-09-17 09:24:53 +0300

    .

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -78,43 +78,41 @@ class ProductController extends Controller
     // }
     public function getProducts(Request $request)
     {
-
         $categoryId = $request->query('category');
         $tagId = $request->query('tag');
-        $limit = $request->query('limit');
-        $isHero = $request->query('only_hero_products', false);
+        $limit = $request->query('limit', null);
+        $isHero = $request->query('only_hero_products', null);
 
 
         $query = Product::query();
 
 
         if ($categoryId) {
-            error_log('category_id');
             $query->where('category_id', $categoryId);
         }
 
 
         if ($tagId) {
-            error_log('tag_id');
             $query->whereHas('tags', function ($tagQuery) use ($tagId) {
                 $tagQuery->where('tag_id', $tagId);
             });
         }
 
+        error_log($isHero);
 
-        if ($isHero) {
-            error_log('is_hero');
-            $query->where('is_hero', true);
+        if ($isHero === '1') {
+
+
+            $query->where('is_hero', 1);
         }
+
 
         $query->with(['tags', 'category', 'images']);
 
 
         if ($limit) {
-            error_log('limit');
             $products = $query->limit($limit)->get();
         } else {
-            error_log('paginate');
             $products = $query->paginate(10);
         }
 
@@ -128,19 +126,18 @@ class ProductController extends Controller
 
     public function storeProduct(Request $request)
     {
-        // Validate the incoming request
         $request->validate([
             'name' => 'required|string|max:255',
             'price' => 'required|numeric|min:0',
             'category_id' => 'required|exists:categories,id',
-            'images.*' => 'nullable|image' // Validation for multiple images
+            'images.*' => 'nullable|image'
         ]);
 
-        // Generate the next unique code (PR0001, PR0002, etc.)
+
         $latestProduct = Product::latest('id')->first();
         $nextCode = $latestProduct ? 'PR' . str_pad($latestProduct->id + 1, 4, '0', STR_PAD_LEFT) : 'PR0001';
 
-        // Create a new product instance
+
         $product = new Product;
         $product->name = $request->name;
         $product->price = $request->price;
@@ -148,7 +145,7 @@ class ProductController extends Controller
         $product->category_id = $request->category_id;
         $product->save();
 
-        // Check if images are provided and store them
+
         if ($request->hasFile('images')) {
             foreach ($request->file('images') as $image) {
 
@@ -161,7 +158,7 @@ class ProductController extends Controller
             }
         }
 
-        // Return a success response
+
         return response()->json([
             'success' => 1,
             'message' => __('messages.product_created_successfully'),
